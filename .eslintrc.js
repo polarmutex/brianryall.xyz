6cf0669ee42d0d7d8172f5856f2f636b0a7625c6
Brian Ryall <brianryall@fastmail.com>
2021-02-09 15:55:16 -0500

    elderjs template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
+  root: true,
   env: {
-    browser: true,
     node: true,
-    es6: true
+    commonjs: false,
+    es2020: true,
   },
-  plugins: ['@typescript-eslint', 'react'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
-  },
-  rules: {
-    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-    '@typescript-eslint/explicit-function-return-type': 'off'
-  },
-  overrides: [
-    // Override some TypeScript rules just for .js files
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off' //
-      }
-    }
-  ]
-};
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
 
+  rules: {
+    'prettier/prettier': 'error',
+    semi: ['error', 'always'],
+    'no-var': ['error'],
+    'no-console': ['off'],
+    'no-unused-vars': ['warn'],
+    'no-mixed-spaces-and-tabs': ['warn'],
+    'node/no-unpublished-require': ['off'],
+  },
+};
