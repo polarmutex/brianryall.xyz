8c415eeeb4e70e00048521616b825c284d78ba88
Brian Ryall <brianryall@fastmail.com>
2021-02-14 00:50:15 -0500

    feat(stats) impl

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,6 +1,7 @@
 <script>
     import header from "../components/header.svelte"
-  export let templateHtml, settings;
+    export let templateHtml, settings;
+    const dev = process.env.NODE_ENV === 'development';
 </script>
 
 <style global lang="postcss">
@@ -11,6 +12,7 @@
 
 <svelte:head>
     <link rel="stylesheet" href="/style.css" />
+    <script async defer data-domain="brianryall.xyz" src="https://stats.brianryall.xyz/js/plausible.js"></script>
 </svelte:head>
 
 <div id="base" class="bg-gruvbox-light-bg dark:bg-gruvbox-dark-bg text-gruvbox-light-fg dark:text-gruvbox-dark-fg" >
